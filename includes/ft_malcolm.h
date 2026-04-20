3ef972adc30abb7609d1536b86624b6aaf84e849
Guillaume Philippe <guphilip@c2r3p1.42lehavre.fr>
2026-04-20 11:32:40 +0200

    ~ | push before continuing in VM

--- includes/ft_malcolm.h
+++ includes/ft_malcolm.h
@@ -3,13 +3,23 @@
 
 #include "libft.h"
 
+/*
+    Structure for ft_malcom : 
+        - src_ip / dest_ip = Arg string "10.12.255.255", will be converted uint32_t for comparison later
+        - src_mac / dest_mac = MAC adresses stocked in 6 bytes array
+        - sockfd =  FD for raw socket 
+        - iface = network interface stocked as a string
+*/
 typedef struct s_malcolm {
-    char    *src_ip;
+    char    *src_ip; 
     uint8_t src_mac[6];
     char    *dest_ip;
     uint8_t dest_mac[6];
     int     sockfd;
     char    iface[IF_NAME_SIZE];
 } t_malcolm;
+
+int parse_ip(const char *str);
+
 
 #endif
