4185ff2c21dbb5308c2d43c8ba72ba54fce4d062
guphilip <guphilip@DONTDIE.myguest.virtualbox.org>
2026-04-22 12:56:08 +0200

    ~ | little fixes here and there

--- src/network/listen.c
+++ src/network/listen.c
@@ -14,14 +14,15 @@ static int is_valid_request(t_malcolm *malcolm_ctx, t_arp_packet *arp)
 	inet_ntop(AF_INET, &arp->target_protocol_addr, target, INET_ADDRSTRLEN);                    // debug
 	printf("op: %d | sender: %s | target: %s\n", ntohs(arp->op_code), sender, target);			// debug
 	printf("expecting target: %s | sender: %s\n", malcolm_ctx->src_ip, malcolm_ctx->dest_ip); 	// debug
-
-
+	
 	if (ntohs(arp->op_code) != 1)
 		return (0);
 	if (arp->target_protocol_addr != src_ip)
 		return (0);
 	if (arp->sender_protocol_addr != dest_ip)
 		return (0);
+
+
 	return (1);
 }
 
@@ -38,9 +39,9 @@ int listen_for_request(t_malcolm *malcolm_ctx)
 		if (len == -1)
 			return (ft_printf(RECV_ERR_MSG, strerror(errno)), 0);
 		
-		printf("sizeof t_eth_header: %zu\n", sizeof(t_eth_header));		// debug
-		printf("sizeof t_arp_packet: %zu\n", sizeof(t_arp_packet));		// debug
-		printf("sizeof t_frame: %zu\n", sizeof(t_frame));				// debug
+		printf("sizeof t_eth_header: %zu\n", sizeof(t_eth_header));								// debug
+		printf("sizeof t_arp_packet: %zu\n", sizeof(t_arp_packet));								// debug
+		printf("sizeof t_frame: %zu\n", sizeof(t_frame));										// debug
 		printf("Received %ld bytes, ethertype: 0x%04x\n", len, ntohs(frame.eth.ether_type));	// debug
 		
 		if (ntohs(frame.eth.ether_type) != ETH_P_ARP)
--- src/network/socket.c
+++ src/network/socket.c
@@ -2,15 +2,20 @@
 
 int init_socket(t_malcolm *malcolm_ctx)
 {
-    malcolm_ctx->sockfd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ARP));
-    if (malcolm_ctx->sockfd == -1)
-        return (ft_printf("ft_malcolm: socket: %s\n", strerror(errno)), 0);
-    if (setsockopt(malcolm_ctx->sockfd, SOL_SOCKET, SO_BINDTODEVICE,
-        malcolm_ctx->iface, IF_NAMESIZE) == -1)
-        return (ft_printf("ft_malcolm: setsockopt: %s\n", strerror(errno)), 0);
-    else
-    {
-        ft_printf("sockfd = %d\n", malcolm_ctx->sockfd);
-    }
-    return (1);
+	struct packet_mreq  mreq;
+	int                 ifindex;
+
+	malcolm_ctx->sockfd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ARP));
+	if (malcolm_ctx->sockfd == -1)
+		return (ft_printf("ft_malcolm: socket: %s\n", strerror(errno)), 0);
+
+	ifindex = if_nametoindex(malcolm_ctx->iface);
+	if (ifindex == 0)
+		return (ft_printf(NAME_TO_IDX_MSG, strerror(errno)), 0);
+	ft_memset(&mreq, 0, sizeof(mreq));
+	mreq.mr_ifindex = ifindex;
+	mreq.mr_type = PACKET_MR_PROMISC;
+	if (setsockopt(malcolm_ctx->sockfd, SOL_SOCKET, PACKET_ADD_MEMBERSHIP, &mreq, sizeof(mreq)) == -1)
+		return (ft_printf("ft_malcolm: setsockopt: %s\n", strerror(errno)), 0);
+	return (1);
 }
