5b5a64ce2720a5064ad84a2966e3b6f1de9be496
Guillaume Philippe <guphilip@c2r5p1.42lehavre.fr>
2026-04-23 14:51:18 +0200

    jpp

--- src/ft_malcolm.c
+++ src/ft_malcolm.c
@@ -12,7 +12,10 @@ int main(int argc, char **argv)
 {
     t_malcolm           malcolm_ctx;
     struct sigaction    sa;
-
+    ft_memset(&sa, 0, sizeof(sa));
+    sa.sa_handler = handle_sigint;
+    sigaction(SIGINT, &sa, NULL);
+    
     if (argc != 5)
         return (ft_printf(BOLD_RED "Usage: ./ft_malcolm <src_ip> <src_mac> <dest_ip> <dest_mac>\n" RESET),  1);
     print_title();
@@ -24,12 +27,6 @@ int main(int argc, char **argv)
     print_sum(&malcolm_ctx);
     find_interface(&malcolm_ctx);
     init_socket(&malcolm_ctx);
-
-
-    ft_memset(&sa, 0, sizeof(sa));
-    sa.sa_handler = handle_sigint;
-    sigaction(SIGINT, &sa, NULL);
-    
     listen_for_request(&malcolm_ctx);
     close(malcolm_ctx.sockfd);
     return (0);
